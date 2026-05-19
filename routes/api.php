c708eb6765c6bdcd6b21450b967d56726bb2acaf
Rhay from Mac <javier.johnrhay@gmail.com>
2026-05-19 09:22:51 +0800

    refaactor: change api message

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health', function () {
     return response()->json([
-        'status' => 'okok',
+        'status' => 'Something has changes within me.',
     ]);
 });
